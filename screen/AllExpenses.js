81d1dc32a8cf6c9d496c0b98a740bc32e6b34ee7
lopera.william@gmail.com <William>
2022-04-27 12:10:36 -0500

    inicio del proyecto

---
+++ screen/AllExpenses.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const AllExpenses = () => {
+  return (
+    <View>
+      <Text>Todos los gastos</Text>
+    </View>
+  );
+};
+
+export default AllExpenses;
+
+const styles = StyleSheet.create({});

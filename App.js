a13d52bf9af5f79e0655b804fe2a1c0de3597bec
lopera.william@gmail.com <William>
2022-04-27 12:39:05 -0500

    agregar navegacion stack y butonTabs

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import ManageExpense from "./screen/ManageExpense";
+import AllExpenses from "./screen/AllExpenses";
+import RecentExpenses from "./screen/RecentExpenses";
+
+const Stack = createNativeStackNavigator();
+const BottonTabs = createBottomTabNavigator();
+
+const ExpensesOverview = () => {
+  return (
+    <BottonTabs.Navigator>
+      <BottonTabs.Screen name="RecentExpenses" component={RecentExpenses} />
+      <BottonTabs.Screen name="AllExpense" component={AllExpenses} />
+    </BottonTabs.Navigator>
+  );
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hola Mundo</Text>
+    <>
       <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="ExpensesOverview" component={ExpensesOverview} />
+          <Stack.Screen name="ManageExpense" component={ManageExpense} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+const styles = StyleSheet.create({});
